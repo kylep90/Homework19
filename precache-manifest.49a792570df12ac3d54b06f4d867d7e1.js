a6f8f5820fc79cfc744dbbac9329876158965e68
Kyle Pingue <pinguek@gmail.com>
2020-08-18 22:43:51 -0500

    Updates

---
+++ precache-manifest.49a792570df12ac3d54b06f4d867d7e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e030ab0266e5eeadfe9172dc931fe7f3",
+    "url": "/Homework19/index.html"
+  },
+  {
+    "revision": "27363a41050a220b9ca8",
+    "url": "/Homework19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a77f082be4acf401857",
+    "url": "/Homework19/static/js/2.26a3c2ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Homework19/static/js/2.26a3c2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27363a41050a220b9ca8",
+    "url": "/Homework19/static/js/main.2841888f.chunk.js"
+  },
+  {
+    "revision": "86dc6248c165947a3a28",
+    "url": "/Homework19/static/js/runtime-main.7b9994cb.js"
+  }
+]);
