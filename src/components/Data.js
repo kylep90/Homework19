91856438ebb92f007f62e6ac4094a0a91121ab79
Kyle Pingue <pinguek@gmail.com>
2020-09-06 21:23:10 -0500

    renders a sorted array without use of the button

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
 import "../assets/style.css"
-
 class Data extends Component {
    state = {
     loading : true,
     users : []
 }; 
+
+sortFunction() {
+    var list = this.state.users
+    console.log(list)
+    list.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1)
+    console.log(list)
+    // console.log(users)
+    // var list = []
+    // users.map(first =>(
+    //     list.push(first.name.first)
+    // ));
+    // console.log(list)
+    // list.sort();
+    // console.log(list)
+}
+
+// function ActionLink() {
+//     function handleClick(e) {
+//       e.preventDefault();
+//       console.log('The link was clicked.');
+//     }
+// }
 
 async componentDidMount(){
     const url = "https://randomuser.me/api/?results=20&nat=gb";
@@ -14,13 +35,15 @@ async componentDidMount(){
     // console.log(data);
     this.setState({users : data.results})
     console.log(this.state.users);
+    
 }
+
 render (){
     return (
         <div>
             <table>
             <tr>
-                <th>Name</th>
+                <th>Name<button onClick={this.sortFunction()}>Sort</button></th>
                 <th>Picture</th>
                 <th>Email</th>
                 <th>Telephone</th>
@@ -31,9 +54,9 @@ render (){
                 {this.state.users.map(user => (
                 <tr key={user.login.uuid}>
                 
-                    
-                    <td><img src={user.picture.thumbnail} alt="pic"></img></td>
                     <td>{user.name.first} {user.name.last}</td>
+                    <td><img src={user.picture.thumbnail} alt="pic"></img></td>
+                    
                     <td>{user.email}</td>
                     <td>{user.cell}</td>
                     <td>{user.dob.date}</td>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,26 +2,34 @@ import React from "react";
 // import "./style.css";
 
 // Using the datalist element we can create autofill suggestions based on the props.breeds array
-function SearchForm(props) {
+function SearchForm() {
+ 
+var search;
+
+function updateSearch(event) {
+  console.log(event.target.value)
+  search=event.target.value;
+}
+
   return (
     <form className="search">
       <div className="form-group">
         <label htmlFor="breed">Search Directory:</label>
-        <input
+        <input type="text" value={search} onChange={updateSearch}
         //   value={props.search}
         //   onChange={props.handleInputChange}
-          name="breed"
-          list="breeds"
-          type="text"
-          className="form-control"
-          placeholder="Type in a name or number to start"
-          id="breed"
+          // name="breed"
+          // list="breeds"
+          
+          // className="form-control"
+          // placeholder="Type in a name or number to start"
+          // id="breed"
         />
-        <datalist id="breeds">
+        {/* <datalist id="breeds"> */}
           {/* {props.breeds.map(breed => (
             <option value={breed} key={breed} />
           ))} */}
-        </datalist>
+        {/* </datalist> */}
         {/* <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success"> */}
           {/* Search */}
         {/* </button> */}
